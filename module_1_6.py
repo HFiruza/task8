f7595fac4dccabcad44fa0fd6945a486f0b446c6
Фируза <ferika2525@gmail.com>
2024-10-17 14:15:23 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,25 @@
+#my_str = {'a', 'b', 'c'}
+#number = 1
+#dictionary = dict.fromkeys(my_dict, number)
+#print(dictionary)
+
+#my_dict = {'apple': 1, 'banan': 2,'peach': 3}
+#print(my_dict)
+#print(my_dict['apple'])
+#my_dict['banan'] = 4
+#print(my_dict['banan'])
+#my_dict.update({'rose': 5,'violet': 6})
+#print(my_dict)
+#a = my_dict.pop('peach')
+#print(a)
+#print(my_dict)
+
+my_set = {'a', 'b', 'c', 1, 2, 3}
+my_list = [1, 1, 1, 2, 2, 3, 3]
+my_list = set(my_list)
+print(set(my_list))
+print(my_list.add('a'))
+print(my_list.add('b'))
+print(my_list)
+print(my_list.discard('a')) #print(my_list.remove('a'))
+print(my_list)
